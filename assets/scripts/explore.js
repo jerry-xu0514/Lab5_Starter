68318ecc6240e8f63ec8c29367da55bab2648236
Jerry Xu <jerriven@gmail.com>
2022-10-31 13:16:52 -0700

    updated explore and fixed smiling face opening

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,7 +3,6 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    console.log("hi")
     const x = document.querySelector("img");
     x.src = "assets/images/smiling.png";
     const speech = window.speechSynthesis;
@@ -14,32 +13,39 @@ function init() {
 }
 
 const speech = window.speechSynthesis;
-
 let sel = document.querySelector("select");
 const input = document.getElementById('text-to-speak');
 let txt = "";
 let click = document.querySelector("button");
+let utterThis = new SpeechSynthesisUtterance(input.value);
+let images = document.querySelector("img");
+
 
 click.addEventListener("click", startSpeaking);
 
 
 
-
 function startSpeaking(e){
     const voices = speech.getVoices();
-
+    images.src = "assets/images/smiling-open.png";
     if(speech.onvoiceschanged !== undefined)
     {
         speech.onvoiceschanged = () => populateVoiceList();
     }
-    const utterThis = new SpeechSynthesisUtterance(input.value);
+    utterThis = new SpeechSynthesisUtterance(input.value);
+    
     const selectedOption = sel.selectedOptions[0].getAttribute('data-name');
     for (let i = 0; i < voices.length ; i++) {
         if (voices[i].name === selectedOption) {
-        utterThis.voice = voices[i];
+            utterThis.voice = voices[i];
         }
     }
     speech.speak(utterThis);
+
+    // images.src = "assets/images/smiling.png";
+    utterThis.onend = (event) => {
+        images.src =  "assets/images/smiling.png";
+    }
 }
 
 function populateVoiceList() {
